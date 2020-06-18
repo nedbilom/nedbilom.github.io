34b593212db5a514ac75ba7d32204cf690ab8680
nedbilom <41346113+nedbilom@users.noreply.github.com>
2020-06-18 16:57:13 +0700

    birthdate add numlock

--- Тестовое задание ПромоГрупп Медиа/main.js
+++ Тестовое задание ПромоГрупп Медиа/main.js
@@ -48,8 +48,6 @@ clearButtons.forEach((cross) => {
 
 birthdate.addEventListener('keydown', (event) => {
 
-  console.log(event.keyCode);
-
   (event.keyCode >= 48 && event.keyCode <= 57)
     || (event.keyCode >= 96 && event.keyCode <= 105)
     || event.keyCode === 110 || event.keyCode === 144
